ab5e881f14119f29244c8d21324decbcba7e44dc
tirechapp <tirechapp@gmail.com>
2021-12-12 16:27:25 -0700

    added Assets and src
    added folder Assets, Spacebound.py, and example.py

---
+++ Spacebound.py
@@ -0,0 +1,76 @@
+import pygame
+import os
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("SPACEBOUND")
+
+WHITE = (255, 255, 255)
+
+FPS = 60
+VEL = 5
+
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+def main():
+    red = pygame.Rect(600, 200, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+    yellow = pygame.Rect(200, 200, SPACESHIP_WIDTH, SPACESHIP_HEIGHT)
+
+    
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        keys_pressed = pygame.key.get_pressed()
+        yellow_handle_movement(keys_pressed, yellow)
+        red_handle_movement(keys_pressed, red)
+        draw_window(red, yellow)
+
+
+    pygame.quit()
+
+
+def draw_window(red, yellow):
+    WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
+    WIN.blit(RED_SPACESHIP, (red.x, red.y))
+    pygame.display.update()
+
+def yellow_handle_movement(keys_pressed, yellow):
+    if keys_pressed[pygame.K_a] : # LEFT
+        yellow.x -= VEL
+    if keys_pressed[pygame.K_d] : # RIGHT
+        yellow.x += VEL
+    if keys_pressed[pygame.K_s] : # DOWN
+        yellow.y += VEL
+    if keys_pressed[pygame.K_w] : # UP
+        yellow.y -= VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] : # LEFT
+        red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] : # RIGHT
+        red.x += VEL
+    if keys_pressed[pygame.K_DOWN] : # DOWN
+        red.y += VEL
+    if keys_pressed[pygame.K_UP] : # UP
+        red.y -= VEL
+
+
+
+
+
+main()
